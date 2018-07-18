f71a66031413b1256b9c582df9eed0a202004e1f
tidues <tidues@gmail.com>
2018-07-18 00:28:01 -0400

    version 1.0.1

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="generalmap",
-    version="1.0.0",
+    version="1.0.1",
     author="Tidues Wei",
     author_email="tidues@gmail.com",
     description="A generalized map function for lift function over structures",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tidues/GeneralMap",
+    url="https://github.com/tidues/GeneralizedMapFunction",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
